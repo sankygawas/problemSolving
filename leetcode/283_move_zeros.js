aa7954bd296e301e2c639e9668e7bb9d326a41a0
sankygawas <sanketgawas1993@gmail.com>
2020-01-23 18:17:02 +0000

    implemenet js solution

---
+++ leetcode/283_move_zeros.js
@@ -0,0 +1,20 @@
+/**
+ * https://leetcode.com/problems/move-zeroes/
+ * 
+ * 
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    
+    let zeroPointer = 0;
+    for(let i=0; i< nums.length; i++){
+        
+        if(nums[i] != 0){
+            let temp = nums[i];
+            nums[i] = nums[zeroPointer]
+            nums[zeroPointer++] = temp
+        }
+     
+    }
+  };
