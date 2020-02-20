49bbb929e49db746c50d805eaa5c6431a4104f38
sankygawas <sanketgawas1993@gmail.com>
2020-02-20 13:05:52 +0000

    change solution

--- coderByte/letter_changes.java
+++ coderByte/letter_changes.java
@@ -1,33 +1,31 @@
 import java.util.*; 
 import java.io.*;
-import java.util.*; 
-import java.io.*;
 
-class Main {  
-  public static String LetterChanges(String str) { 
-  
-  StringBuffer buffer = new StringBuffer();
-  int temp;
-   for(int i=0;i<str.length();i++){
-       temp = str.charAt(i);
-       if((temp>=97 && temp<=122)||(temp>=65 && temp<=90)){
-       temp++;
-       }
-       if((char)temp == 'a' || (char)temp == 'e' || (char)temp == 'i' || (char)temp == 'o' || (char)temp == 'u')
-       temp-=32;
-       
-       buffer.append((char)temp);
-      
-   }
-       
-    return buffer.toString();
-    
-  } 
-  
+class Main {
+
+  public static String LetterChanges(String str) {
+    // code goes here  
+    StringBuilder builder = new StringBuilder();
+    for(int i=0;i<str.length();i++){
+
+      if(str.charAt(i) >= 97 && str.charAt(i) <= 122){
+      char character = (char)((str.charAt(i) % 122)+1);
+      if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u'){
+        builder.append((character + "").toString().toUpperCase());
+      }
+      else
+        builder.append(character);
+    }
+    else
+      builder.append(str.charAt(i));
+    }
+    return builder.toString();
+  }
+
   public static void main (String[] args) {  
     // keep this function call here     
     Scanner s = new Scanner(System.in);
     System.out.print(LetterChanges(s.nextLine())); 
-  }   
-  
+  }
+
 }
